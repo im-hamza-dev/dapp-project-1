7ec636214d3d6c11319af9c8dd1176f59c862ee4
Hamza Iqbal <hiqbal.bese16seecs@gmail.com>
2022-08-02 00:58:05 +0500

    contract deployed by wave portal and metamask account connected with this react app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [currentAccount, setCurrentAccount] = useState();
+
+  const checkIfMetamaskInstalled = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Metamask not installed...");
+        return;
+      } else {
+        console.log("Metamask installed: ", ethereum);
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Got this authorized account: ", account);
+          setCurrentAccount(account);
+        } else {
+          console.log("No account found!");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get Metamask!");
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected Account: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfMetamaskInstalled();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">👋 Hey there!</div>
+
+        <div className="bio">
+          I am Hamza and I worked on self-driving cars so that's pretty cool
+          right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={connectWallet}>
+          {currentAccount ? "Connected" : "Connect Metamask"}
+        </button>
+
+        {currentAccount && <div className="bio">{currentAccount}</div>}
+      </div>
     </div>
   );
 }
-
-export default App;
